3da19928be0959b02c4c159e051ec2c4e5be8fe2
GilRahamim98 <gilrahamimgr@gmail.com>
2023-07-29 01:47:26 +0300

    feat :added feed and all the component and data that was needed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import {useState,useEffect} from 'react';
+import {Stack,Box,Typography} from '@mui/material'
+
+import {SideBar,Videos} from './';
+import { fetchFromAPI } from '../utils/fetchFromApi';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos,setVideos]=useState([]);
+
+  useEffect(()=>{
+    const fetchVideos=async()=>{
+      const data=await fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+      setVideos(data.items)
+    }
+    fetchVideos()
+
+  },[selectedCategory])
+
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection:{xs:"column",md:"row"}}}>
+      <Box sx={{height:{xs:'auto',md:'92vh'},borderRight:'1px solid white',px:{xs:0,md:2}}}>
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
+        <Typography className='copyright' variant='body2' sx={{mt:1.5,color:'white'}}>
+          Made by Gil Rahamim
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#7d12ff" }}>videos</span>
+        </Typography>
+        <Videos videos={videos}/>
+      </Box>
+    </Stack>
   )
 }
 
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,6 @@
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import HomeIcon from '@mui/icons-material/Home';
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball'
 import CodeIcon from '@mui/icons-material/Code';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
@@ -13,21 +14,22 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
 export const logo = "https://i.ibb.co/B6V61xY/play-button-icon-isolated-on-black-background-play-icon-neon-sign-play-button-icon-neon-animation-pl.jpg";
-// export const categories = [
-//   { name: 'New', icon: <HomeIcon />, },
-//   { name: 'Coding', icon: <CodeIcon />, },
-//   { name: 'ReactJS', icon: <CodeIcon />, },
-//   { name: 'NextJS', icon: <CodeIcon />, },
-//   { name: 'Music', icon: <MusicNoteIcon /> },
-//   { name: 'Education', icon: <SchoolIcon />, },
-//   { name: 'Podcast', icon: <GraphicEqIcon />, },
-//   { name: 'Movie', icon: <OndemandVideoIcon />, },
-//   { name: 'Gaming', icon: <SportsEsportsIcon />, },
-//   { name: 'Live', icon: <LiveTvIcon />, },
-//   { name: 'Sport', icon: <FitnessCenterIcon />, },
-//   { name: 'Fashion', icon: <CheckroomIcon />, },
-//   { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
-//   { name: 'Comedy', icon: <TheaterComedyIcon />, },
-//   { name: 'Gym', icon: <FitnessCenterIcon />, },
-//   { name: 'Crypto', icon: <DeveloperModeIcon />, },
-// ];
+export const categories = [
+  { name: 'New', icon: <HomeIcon /> , },
+  { name: 'NBA', icon: <SportsBasketballIcon /> , },
+  { name: 'Coding', icon: <CodeIcon/> , },
+  { name: 'ReactJS', icon: <CodeIcon/> , },
+  { name: 'NextJS', icon: <CodeIcon/> , },
+  { name: 'Music', icon: <MusicNoteIcon/>  },
+  { name: 'Education', icon: <SchoolIcon/> , },
+  { name: 'Podcast', icon: <GraphicEqIcon/> , },
+  { name: 'Movie', icon: <OndemandVideoIcon/> , },
+  { name: 'Gaming', icon: <SportsEsportsIcon/> , },
+  { name: 'Live', icon: <LiveTvIcon/> , },
+  { name: 'Sport', icon: <FitnessCenterIcon/> , },
+  { name: 'Fashion', icon: <CheckroomIcon/> , },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon/> , },
+  { name: 'Comedy', icon: <TheaterComedyIcon/> , },
+  { name: 'Gym', icon: <FitnessCenterIcon/> , },
+  { name: 'Crypto', icon: <DeveloperModeIcon/> , },
+];
