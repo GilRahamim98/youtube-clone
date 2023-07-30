f8a0d058ae484f0233759670b39aa25e90111eed
GilRahamim98 <gilrahamimgr@gmail.com>
2023-07-30 10:42:57 +0300

    feat:changed the categories in the side menu

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -5,11 +5,10 @@ import CodeIcon from '@mui/icons-material/Code';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import LiveTvIcon from '@mui/icons-material/LiveTv';
-import SchoolIcon from '@mui/icons-material/School';
-import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
-import CheckroomIcon from '@mui/icons-material/Checkroom';
-import GraphicEqIcon from '@mui/icons-material/GraphicEq';
-import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
+import PetsIcon from '@mui/icons-material/Pets';
+import FlightIcon from '@mui/icons-material/Flight';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
@@ -18,18 +17,15 @@ export const categories = [
   { name: 'New', icon: <HomeIcon /> , },
   { name: 'NBA', icon: <SportsBasketballIcon /> , },
   { name: 'Coding', icon: <CodeIcon/> , },
-  { name: 'ReactJS', icon: <CodeIcon/> , },
-  { name: 'NextJS', icon: <CodeIcon/> , },
+  { name: 'Travel', icon: <FlightIcon/> , },
   { name: 'Music', icon: <MusicNoteIcon/>  },
-  { name: 'Education', icon: <SchoolIcon/> , },
-  { name: 'Podcast', icon: <GraphicEqIcon/> , },
+  { name: 'Stocks', icon: <ShowChartIcon/> , },
+  { name: 'Pets', icon: <PetsIcon/> , },
   { name: 'Movie', icon: <OndemandVideoIcon/> , },
   { name: 'Gaming', icon: <SportsEsportsIcon/> , },
   { name: 'Live', icon: <LiveTvIcon/> , },
   { name: 'Sport', icon: <FitnessCenterIcon/> , },
-  { name: 'Fashion', icon: <CheckroomIcon/> , },
-  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon/> , },
-  { name: 'Comedy', icon: <TheaterComedyIcon/> , },
+  { name: 'Cars', icon: <TimeToLeaveIcon/> , },
   { name: 'Gym', icon: <FitnessCenterIcon/> , },
   { name: 'Crypto', icon: <DeveloperModeIcon/> , },
 ];
